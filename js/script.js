a575261099be03076aaeddc243358686ab7d4ac3
Artem <artem141221@gmail.com>
2021-05-15 11:17:52 +0300

    work

--- js/script.js
+++ js/script.js
@@ -13,12 +13,12 @@ fetch(myRequest)
         let blockAuto = document.querySelector('.block_info_auto');
 
         const photo = {
-            1: "/images/1.jpg",
-            2: "/images/2.jpg",
-            3: "/images/3.jpg",
-            4: "/images/4.jpg",
-            5: "/images/5.jpg",
-            6: "/images/6.jpg"
+            1: "./images/1.jpg",
+            2: "./images/2.jpg",
+            3: "./images/3.jpg",
+            4: "./images/4.jpg",
+            5: "./images/5.jpg",
+            6: "./images/6.jpg"
         }
     
         headers.innerHTML = `
